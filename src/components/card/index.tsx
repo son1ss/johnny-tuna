588a91641d59d92d8bf501cb6f645cffe859bade
son1s <fomin.ea2003@gmail.com>
2023-10-02 07:36:45 +0300

    fix: removed deprecated image

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Card(props: Card) {
   return (
     <div className={styles.card}>
-      <Image className={styles.image} src={props.image} alt={props.title} width={190} height={240} objectFit="cover" />
+      <Image className={styles.image} src={props.image} alt={props.title} width={190} height={240} />
       <div className={styles.controls}>
         <div className={styles.info}>
           <h3 className={styles.title}>{props.title}</h3>
